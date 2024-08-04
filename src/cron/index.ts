5c5afaf0ac5899052b4aee9f128b8df1b0213a21
Nonthasak Laoluerat <nonthasak.l@gmail.com>
2024-08-04 23:26:18 +0700

    upd: add more game

--- src/cron/index.ts
+++ src/cron/index.ts
@@ -1,17 +1,18 @@
 import { CronJob } from "cron";
 import { resetPointByRound } from "../sanity/services/reset.service";
 import dayjs from "dayjs";
+import { jobData } from "./jobData";
 
 export const job = new CronJob(
   "*/1 * * * *",
   async () => {
-    console.log("processing");
-    console.log("@server time: ", dayjs().format());
-    console.log("@local time: ", dayjs().tz("Asia/Bangkok").format());
-
-    const contractAddress = "0x07B2bCc269B100b51AB8598d44AB568C7199C7BC";
-    const gameId = "3fc396c8-b968-46fd-849d-d2243102fe00";
-    await resetPointByRound(gameId, contractAddress);
+    for (let i = 0; i < jobData.length; i++) {
+      console.log("processing index: #", i);
+      console.log("@server time: ", dayjs().format());
+      console.log("@local time: ", dayjs().tz("Asia/Bangkok").format());
+      console.log("@gameId: ", jobData[i].gameId);
+      await resetPointByRound(jobData[i].gameId, jobData[i].contractAddress);
+    }
   },
   null,
   false,
---
+++ src/cron/jobData.ts
@@ -0,0 +1,10 @@
+export const jobData = [
+  {
+    gameId: "3fc396c8-b968-46fd-849d-d2243102fe00",
+    contractAddress: "0x07B2bCc269B100b51AB8598d44AB568C7199C7BC",
+  },
+  {
+    gameId: "2dd89b33-cb78-4fda-86f3-0aaa6a861409",
+    contractAddress: "0xDbcA383a5155cbE4BDD3404695470BaeA8e1Bb22",
+  },
+];
